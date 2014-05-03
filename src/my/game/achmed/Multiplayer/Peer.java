c4de5e9dca93b52a12e63a245ec4c76f10f6ba47
Sérgio Alves <sergio2540@gmail.com>
2014-05-03 15:12:38 +0100

    merge braga

---
+++ src/my/game/achmed/Multiplayer/Peer.java
@@ -0,0 +1,31 @@
+package my.game.achmed.Multiplayer;
+
+public class Peer {
+	
+	
+	private String peerName;
+	private String peerAddress;
+	
+	
+	public Peer(String name, String add){
+		
+		peerName = name;
+		peerAddress = add;
+
+	}
+
+	
+	public String getPeerName() {
+		return peerName;
+	}
+
+	
+	public String getPeerAddress() {
+		return peerAddress;
+	}
+
+	public String toString(){
+		return peerName;
+		
+	}
+}
